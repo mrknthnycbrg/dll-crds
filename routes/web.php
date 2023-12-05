4d0078bf1c34a4b81b43d760601ceb0db76864f0
mrknthnycbrg <macabriga@outlook.com>
2023-12-05 22:28:08 +0800

    Edited routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\ViewFile;
 use App\Livewire\Ask\AskPage;
 use App\Livewire\Downloadables\AllDownloadables;
+use App\Livewire\Downloadables\ShowDownloadable;
 use App\Livewire\Home\HomePage;
 use App\Livewire\Posts\AllPosts;
+use App\Livewire\Posts\CategoryPosts;
 use App\Livewire\Posts\ShowPost;
 use App\Livewire\Researches\AllResearches;
+use App\Livewire\Researches\DepartmentResearches;
 use App\Livewire\Researches\ShowResearch;
 use App\Livewire\Welcome\WelcomePage;
 use Illuminate\Support\Facades\Route;
@@ -32,9 +35,12 @@ Route::middleware([
     Route::get('/home', HomePage::class)->name('home');
     Route::get('/news', AllPosts::class)->name('all-posts');
     Route::get('/news/{slug}', ShowPost::class)->name('show-post');
+    Route::get('/news/category/{slug}', CategoryPosts::class)->name('category-posts');
     Route::get('/researches', AllResearches::class)->name('all-researches');
     Route::get('/researches/{slug}', ShowResearch::class)->name('show-research');
+    Route::get('/researches/department/{slug}', DepartmentResearches::class)->name('department-researches');
     Route::get('/researches/file/{slug}', ViewFile::class)->name('view-file');
     Route::get('/resources', AllDownloadables::class)->name('all-downloadables');
+    Route::get('/resources/{slug}', ShowDownloadable::class)->name('show-downloadable');
     Route::get('/ask', AskPage::class)->name('ask');
 });
