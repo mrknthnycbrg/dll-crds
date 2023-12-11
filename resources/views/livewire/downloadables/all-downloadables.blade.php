4643ef2a6e8b3c1918f475ed6f10c204e9fa2919
mrknthnycbrg <macabriga@outlook.com>
2023-12-11 09:33:17 +0800

    Edited Blade views

--- resources/views/livewire/downloadables/all-downloadables.blade.php
+++ resources/views/livewire/downloadables/all-downloadables.blade.php
@@ -6,6 +6,20 @@
     </x-slot>
 
     <div class="mx-auto max-w-full px-4 py-8 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 gap-x-8 md:grid-cols-3">
+            <div class="mb-8">
+                <label class="block text-sm font-medium text-gray-700" for="year">{{ 'Filter by Year' }}</label>
+                <select
+                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-900 focus:ring-blue-900"
+                    id="year" wire:model.live.debounce="selectedYear">
+                    <option value="0" selected>{{ 'All Years' }}</option>
+                    @for ($year = today()->year; $year >= 2001; $year--)
+                        <option value="{{ $year }}">{{ $year }}</option>
+                    @endfor
+                </select>
+            </div>
+        </div>
+
         <div class="grid grid-cols-1 gap-8 md:grid-cols-3">
             @forelse ($downloadables as $downloadable)
                 <a class="group block aspect-auto w-full space-y-2 rounded-md bg-gray-50 p-4 shadow-lg hover:bg-blue-50"
--- resources/views/livewire/posts/category-posts.blade.php
+++ resources/views/livewire/posts/category-posts.blade.php
@@ -10,10 +10,25 @@
     </x-slot>
 
     <div class="mx-auto max-w-full px-4 py-8 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 gap-x-8 md:grid-cols-3">
+            <div class="mb-8">
+                <label class="block text-sm font-medium text-gray-700" for="year">{{ 'Filter by Year' }}</label>
+                <select
+                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-900 focus:ring-blue-900"
+                    id="year" wire:model.live.debounce="selectedYear">
+                    <option value="0" selected>{{ 'All Years' }}</option>
+                    @for ($year = today()->year; $year >= 2001; $year--)
+                        <option value="{{ $year }}">{{ $year }}</option>
+                    @endfor
+                </select>
+            </div>
+        </div>
+
         <div class="grid grid-cols-1 gap-8 md:grid-cols-3">
             @forelse ($posts as $post)
                 <a class="group block aspect-auto w-full space-y-2 rounded-md bg-gray-50 p-4 shadow-lg hover:bg-blue-50"
-                    href="{{ route('show-post', ['slug' => $post->slug]) }}" wire:navigate wire:key="{{ $post->id }}">
+                    href="{{ route('show-post', ['slug' => $post->slug]) }}" wire:navigate
+                    wire:key="{{ $post->id }}">
                     @if ($post->image_path)
                         <img class="mx-auto aspect-video w-full rounded-md object-cover"
                             src="{{ $post->formattedImage() }}" alt="{{ $post->title }}">
