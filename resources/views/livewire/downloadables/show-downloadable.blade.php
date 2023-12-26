7d7c57f4a5a63e562fa002e59f70eff72c296ec7
mrknthnycbrg <macabriga@outlook.com>
2023-12-26 19:10:50 +0800

    Edited Blade views

--- resources/views/livewire/downloadables/show-downloadable.blade.php
+++ resources/views/livewire/downloadables/show-downloadable.blade.php
@@ -1,5 +1,5 @@
 <div class="mx-auto max-w-full bg-white px-4 py-8 sm:px-6 lg:px-8">
-    <div class="mx-auto max-w-full space-y-4">
+    <div class="max-w-full space-y-4">
         <h1 class="text-4xl font-black text-gray-900">
             {{ $downloadable->name }}
         </h1>
--- resources/views/livewire/researches/show-research.blade.php
+++ resources/views/livewire/researches/show-research.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-auto max-w-full bg-white px-4 py-8 sm:px-6 lg:px-8">
     <div class="grid grid-cols-1 gap-8 md:grid-cols-3">
-        <div class="mx-auto max-w-full space-y-4 md:col-span-2">
+        <div class="max-w-full space-y-4 md:col-span-2">
             <h1 class="text-4xl font-black text-gray-900">
                 {{ $research->title }}
             </h1>
