2dca9515191868ff29fe4f28e548f859ea6b5b30
mrknthnycbrg <macabriga@outlook.com>
2023-12-30 20:40:46 +0800

    Edited routes

--- routes/jetstream.php
+++ routes/jetstream.php
@@ -1,13 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Jetstream\Http\Controllers\CurrentTeamController;
-use Laravel\Jetstream\Http\Controllers\Livewire\ApiTokenController;
 use Laravel\Jetstream\Http\Controllers\Livewire\PrivacyPolicyController;
-use Laravel\Jetstream\Http\Controllers\Livewire\TeamController;
 use Laravel\Jetstream\Http\Controllers\Livewire\TermsOfServiceController;
 use Laravel\Jetstream\Http\Controllers\Livewire\UserProfileController;
-use Laravel\Jetstream\Http\Controllers\TeamInvitationController;
 use Laravel\Jetstream\Jetstream;
 
 Route::group(['middleware' => config('jetstream.middleware', ['web'])], function () {
@@ -27,23 +23,5 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
     Route::group(['middleware' => array_values(array_filter([$authMiddleware, $authSessionMiddleware]))], function () {
         // User & Profile...
         Route::get('/user/profile', [UserProfileController::class, 'show'])->name('profile.show');
-
-        Route::group(['middleware' => 'verified'], function () {
-            // API...
-            if (Jetstream::hasApiFeatures()) {
-                Route::get('/user/api-tokens', [ApiTokenController::class, 'index'])->name('api-tokens.index');
-            }
-
-            // Teams...
-            if (Jetstream::hasTeamFeatures()) {
-                Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
-                Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
-                Route::put('/current-team', [CurrentTeamController::class, 'update'])->name('current-team.update');
-
-                Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
-                    ->middleware(['signed'])
-                    ->name('team-invitations.accept');
-            }
-        });
     });
 });
