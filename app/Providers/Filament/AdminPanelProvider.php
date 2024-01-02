922a9007dcc3bda09c269cd73c07bb801b5d6406
mrknthnycbrg <macabriga@outlook.com>
2024-01-02 10:22:54 +0800

    Edited providers

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,7 @@ class AdminPanelProvider extends PanelProvider
                 'Settings',
             ])
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => Color::hex('#1e40af'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
