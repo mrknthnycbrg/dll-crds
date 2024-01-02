3c6e13b524a81969e263d6000c9a2970ff8fc7f1
mrknthnycbrg <macabriga@outlook.com>
2024-01-02 15:38:57 +0800

    Edited config

--- config/filament-logger.php
+++ config/filament-logger.php
@@ -22,14 +22,14 @@ return [
     ],
 
     'notifications' => [
-        'enabled' => true,
+        'enabled' => false,
         'logger' => \Z3d0X\FilamentLogger\Loggers\NotificationLogger::class,
         'color' => 'success',
         'log_name' => 'Notification',
     ],
 
     'models' => [
-        'enabled' => true,
+        'enabled' => false,
         'log_name' => 'Model',
         'color' => 'warning',
         'logger' => \Z3d0X\FilamentLogger\Loggers\ModelLogger::class,
